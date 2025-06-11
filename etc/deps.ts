353078f92897bcf7ea8ff2eb7d70ab7cfb1c1651
jimz@northern-sucre-joggle <mail@jimz.cc>
2025-06-11 22:03:42 +0800

    cleanup and focus on jsr

--- etc/deps.ts
+++ etc/deps.ts
@@ -8,9 +8,11 @@
 // remote libraries
 // -------------------
 // deno standard library
-export { assert } from "jsr:@std/assert@^1.0.4";
+export { 
+  assert 
+} from "jsr:@std/assert@^1.0.13";
 
 // supporting util from dallmo
 //export { dallmo_util_yaml } from "https://deno.land/x/dallmo_util_yaml/mod.ts";
-export * as dallmo_util_array from "jsr:@dallmo/util-array@^1.3.0";
-export * as dallmo_util_math  from "jsr:@dallmo/util-math@^1.2.0";
+export * as dallmo_util_array from "jsr:@dallmo/util-array@^1.4.0";
+export * as dallmo_util_math  from "jsr:@dallmo/util-math@^1.3.0";
--- sandbox/README.md
+++ sandbox/README.md
@@ -1,5 +1,3 @@
 # overview
 
 test 2 approaches, time both, and see which is more efficient.
-
-
